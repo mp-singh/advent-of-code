b526172441cd47202d381a3c867bb3346ca85ca5
Prince <mandeep@mandeepsingh.ca>
2022-12-06 22:53:10 +0000

    cleanup

--- 2022/day6/src/lib.rs
+++ 2022/day6/src/lib.rs
@@ -5,21 +5,14 @@ pub fn parse_input() -> String {
 }
 
 pub fn part1(input: &str) -> usize {
-    let sliding_window = 4;
-    for i in 0..(input.len() - 1 - sliding_window) {
-        let chunk = &mut input[i..i + sliding_window]
-            .chars()
-            .unique()
-            .collect::<Vec<char>>();
-        if chunk.len() == sliding_window {
-            return i + sliding_window;
-        }
-    }
-    0
+    find_marker(input, 4)
 }
 
 pub fn part2(input: &str) -> usize {
-    let sliding_window = 14;
+    find_marker(input, 14)
+}
+
+fn find_marker(input: &str, sliding_window: usize) -> usize {
     for i in 0..(input.len() - 1 - sliding_window) {
         let chunk = &mut input[i..i + sliding_window]
             .chars()
