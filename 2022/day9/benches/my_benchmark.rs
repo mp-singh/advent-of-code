e52cbe0e8cb38b7a46da6fd2fb5d1cda739aafe5
Prince <mandeep@mandeepsingh.ca>
2022-12-12 03:33:32 +0000

    day9 part2

--- 2022/day9/benches/my_benchmark.rs
+++ 2022/day9/benches/my_benchmark.rs
@@ -1,13 +1,13 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use day9::{parse_input, part1};
+use day9::{parse_input, part1, part2};
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("day9-part1", |b| {
         b.iter(|| part1(black_box(&parse_input())))
     });
-    // c.bench_function("day8-part2", |b| {
-    //     b.iter(|| part2(black_box(&parse_input())))
-    // });
+    c.bench_function("day9-part2", |b| {
+        b.iter(|| part2(black_box(&parse_input())))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
--- 2022/day9/src/lib.rs
+++ 2022/day9/src/lib.rs
@@ -14,50 +14,89 @@ pub fn part1(input: &str) -> usize {
         .map(Instruction::from_str)
         .collect::<Vec<_>>();
 
-    let mut head = Point::new(0, 0);
-    let mut tail = Point::new(0, 0);
+    let mut snake = [Point { row: 0, col: 0 }; 2].to_vec();
     let mut visited: HashSet<Point> = HashSet::new();
-    visited.insert(tail);
 
-    instructions
-        .iter()
-        .for_each(|instruction| match instruction {
+    for instruction in instructions {
+        match instruction {
             Instruction::Move(direction, distance) => {
-                for _ in 0..*distance {
-                    match direction {
-                        Direction::Up => {
-                            head = Point::new(head.row - 1, head.col);
-                            if (head.row - tail.row).abs() > 1 {
-                                tail = Point::new(tail.row - 1, head.col);
-                                visited.insert(tail);
-                            }
-                        }
-                        Direction::Down => {
-                            head = Point::new(head.row + 1, head.col);
-                            if (head.row - tail.row).abs() > 1 {
-                                tail = Point::new(tail.row + 1, head.col);
-                                visited.insert(tail);
-                            }
-                        }
-                        Direction::Left => {
-                            head = Point::new(head.row, head.col - 1);
-                            if (head.col - tail.col).abs() > 1 {
-                                tail = Point::new(head.row, tail.col - 1);
-                                visited.insert(tail);
-                            }
-                        }
-                        Direction::Right => {
-                            head = Point::new(head.row, head.col + 1);
-                            if (head.col - tail.col).abs() > 1 {
-                                tail = Point::new(head.row, tail.col + 1);
-                                visited.insert(tail);
-                            }
-                        }
-                    }
+                for _ in 0..distance {
+                    move_snake(&mut snake, &direction, &mut visited);
                 }
             }
-        });
+        }
+    }
     visited.len()
+}
+
+pub fn part2(input: &str) -> usize {
+    let instructions = input
+        .split('\n')
+        .map(Instruction::from_str)
+        .collect::<Vec<_>>();
+
+    let mut snake = [Point { row: 0, col: 0 }; 10].to_vec();
+    let mut visited: HashSet<Point> = HashSet::new();
+    visited.insert(snake[0]);
+    for instruction in instructions {
+        match instruction {
+            Instruction::Move(direction, distance) => {
+                for _ in 0..distance {
+                    move_snake(&mut snake, &direction, &mut visited);
+                }
+            }
+        }
+    }
+    visited.len()
+}
+
+fn move_snake(snake: &mut Vec<Point>, direction: &Direction, visited: &mut HashSet<Point>) {
+    let head = snake[0];
+    snake[0] = match direction {
+        Direction::Up => Point::new(head.row - 1, head.col),
+        Direction::Down => Point::new(head.row + 1, head.col),
+        Direction::Left => Point::new(head.row, head.col - 1),
+        Direction::Right => Point::new(head.row, head.col + 1),
+    };
+
+    for i in 1..snake.len() {
+        let head = snake[i - 1];
+        let mut tail = snake[i];
+        let row_diff = (head.row - tail.row).abs();
+        let col_diff = (head.col - tail.col).abs();
+        if row_diff > col_diff {
+            if head.row - tail.row > 1 {
+                tail = Point::new(tail.row + 1, head.col);
+            }
+            if tail.row - head.row > 1 {
+                tail = Point::new(tail.row - 1, head.col);
+            }
+        } else if row_diff < col_diff {
+            if head.col - tail.col > 1 {
+                tail = Point::new(head.row, tail.col + 1);
+            }
+            if tail.col - head.col > 1 {
+                tail = Point::new(head.row, tail.col - 1);
+            }
+        } else if row_diff > 1 {
+            if head.row - tail.row > 1 {
+                tail = Point::new(tail.row + 1, tail.col);
+            }
+            if tail.row - head.row > 1 {
+                tail = Point::new(tail.row - 1, tail.col);
+            }
+            if head.col - tail.col > 1 {
+                tail = Point::new(tail.row, tail.col + 1);
+            }
+            if tail.col - head.col > 1 {
+                tail = Point::new(tail.row, tail.col - 1);
+            }
+        }
+        snake[i] = tail;
+        if i == snake.len() - 1 {
+            visited.insert(tail);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -75,5 +114,17 @@ mod test {
         L 5
         R 2";
         assert_eq!(part1(input), 13);
+    }
+    #[test]
+    fn test_part2() {
+        let input = "R 5
+        U 8
+        L 8
+        D 3
+        R 17
+        D 10
+        L 25
+        U 20";
+        assert_eq!(part2(input), 36);
     }
 }
